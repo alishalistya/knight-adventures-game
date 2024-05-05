c56cac63de54a939f05a3fe83891a5730f4b11b3
arsaizdihar <arsadihar@gmail.com>
2024-05-05 15:01:50 +0700

    fix: attack hitbox and trigger

--- Assets/Scripts/Player/GraphicsEvent.cs
+++ Assets/Scripts/Player/GraphicsEvent.cs
@@ -18,6 +18,7 @@ public class GraphicsEvent : MonoBehaviour, IWeaponAnimationHandler
 
     public void OnStartAttackTrigger()
     {
+        Debug.Log("OnStartAttackTrigger");
         if (player.Inventory.CurrentWeapon is RangedWeapon weapon)
         {
             weapon.StartProjectile(player);
@@ -30,6 +31,7 @@ public class GraphicsEvent : MonoBehaviour, IWeaponAnimationHandler
 
     public void OnEndAttackTrigger()
     {
+        Debug.Log("OnEndAttackTrigger");
         if (player.Inventory.CurrentWeapon is MeleeWeapon meleeWeapon)
         {
             meleeWeapon.IsActive = false;
