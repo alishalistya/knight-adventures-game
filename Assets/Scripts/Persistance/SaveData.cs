2ae67cae445ef7f28d2b16d98ee735a41a502777
arsaizdihar <arsadihar@gmail.com>
2024-05-07 20:09:55 +0700

    feat: add button prefab

--- Assets/Scripts/Persistance/SaveData.cs
+++ Assets/Scripts/Persistance/SaveData.cs
@@ -2,4 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SaveData { }
+[System.Serializable]
+public class SaveData
+{
+  GameStatistics statistics;
+  int playerHealth;
+  int playerGold;
+  int sceneNumber;
+  int questNumber;
+
+  float[] position;
+  float[] rotation;
+
+  int weaponIndex;
+
+  public SaveData(GameStatistics statistics, int playerHealth, int playerGold, int sceneNumber, int questNumber, Vector3 position, Vector3 rotation, int weaponIndex)
+  {
+    this.statistics = statistics;
+    this.playerHealth = playerHealth;
+    this.playerGold = playerGold;
+    this.sceneNumber = sceneNumber;
+    this.questNumber = questNumber;
+    this.position = new float[] { position.x, position.y, position.z };
+    this.rotation = new float[] { rotation.x, rotation.y, rotation.z };
+    this.weaponIndex = weaponIndex;
+  }
+}
