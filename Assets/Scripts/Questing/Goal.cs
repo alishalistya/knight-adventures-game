64453eec2b426160f7a10c3cb615a2c6e09cd2ea
Fakhrimm <13521045@std.stei.itb.ac.id>
2024-05-09 20:30:46 +0700

    fix: goal areagoal bug

--- Assets/Scripts/Questing/Goal.cs
+++ Assets/Scripts/Questing/Goal.cs
@@ -19,6 +19,10 @@ public class Goal
         {
             Complete();
         }
+        else 
+        {
+            Completed = false;
+        }
         QuestEvents.GoalProgressed(Quest);
     }
 
