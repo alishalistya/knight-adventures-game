92d111bd62f14ae5a4b0258b38fbb494ca6c5973
Alisha Listya <73476678+alishalistyaa@users.noreply.github.com>
2024-05-09 23:18:26 +0700

    add: keroco on quest-4

---
+++ Assets/Scripts/Questing/Quests/Quest4.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quest4 : Quest
+{
+    public void Awake()
+    {
+        QuestName = "Dead King's Rise";
+        Description = "Defeat the King.";
+        GoldReward = 100;
+
+        Goals.Add(new KillGoal(this, 0, "Kill 4 kerocos", false, 0, 4));
+
+        Goals.Add(new KillGoal(this, 1, "Kill 2 Kepala Keroco", false, 0, 2));
+
+        Goals.Add(new KillGoal(this, 2, "Kill 2 Jendral", false, 0, 2));
+
+        Goals.Add(new KillGoal(this, 3, "Kill the King", false, 0, 1));
+
+
+        Goals.ForEach(g => g.Init());
+    }
+
+    protected override void GiveReward()
+    {
+        Debug.Log("Quest4 Completed");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
