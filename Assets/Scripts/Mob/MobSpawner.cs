33e142f063f491f8b59b69f43c1aa730ab4de612
Fakhrimm <13521045@std.stei.itb.ac.id>
2024-05-10 00:54:15 +0700

    feat: finalize Quest2 and bake scene 3

---
+++ Assets/Scripts/Mob/MobSpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject mobPrefab;
+
+    void Awake()
+    {
+        CombatEvents.OnMobKilled += SpawnMob;
+    }
+    
+    void SpawnMob(Vector3 position, Mob mob)
+    {
+        Instantiate(mobPrefab, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Questing/Quests/Quest2.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quest2 : Quest
+{
+    public void Awake()
+    {
+        QuestName = "Kill all the kerocos";
+        Description = "Destroy the kerocos to save the town";
+        GoldReward = 20;
+        Goals.Add(new KillGoal(this, 0, "Kill 20 kerocos", false, 0, 5));
+        Goals.Add(new AreaGoal(this, "LocationArea", "Stand in front of the door", false, 0, 1));
+        Goals.ForEach(g => g.Init());
+    }
+
+    protected override void GiveReward()
+    {
+        Debug.Log("Quest2 Completed");
+        SceneManager.LoadScene("Quest-3");
+    }
+}
