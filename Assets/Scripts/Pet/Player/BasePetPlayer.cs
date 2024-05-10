a761cb7af360f6257d2e66a043cf9c32486d291b
arsaizdihar <arsadihar@gmail.com>
2024-05-10 09:41:38 +0700

    feat: add full hp pet cheat

--- Assets/Scripts/Pet/Player/BasePetPlayer.cs
+++ Assets/Scripts/Pet/Player/BasePetPlayer.cs
@@ -1,5 +1,13 @@
 using System;
 
-public abstract class BasePetPlayer: BasePet<Player>
+public abstract class BasePetPlayer : BasePet<Player>
 {
+  public override void TakeDamage(int amount)
+  {
+    if (PlayerCheats.IsCheat(StatusCheats.FULL_HP_PET))
+    {
+      return;
+    }
+    base.TakeDamage(amount);
+  }
 }
